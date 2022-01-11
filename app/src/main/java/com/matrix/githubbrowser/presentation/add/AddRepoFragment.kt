6025968a0f8014039d578b2268dc8976441779e2
it5prasoon <prasoonk187@gmail.com>
2022-01-11 10:30:34 +0530

    :fire: application is now able to get repository description using retrofit and now doing checks for availability of repository

--- app/src/main/java/com/matrix/githubbrowser/presentation/add/AddRepoFragment.kt
+++ app/src/main/java/com/matrix/githubbrowser/presentation/add/AddRepoFragment.kt
@@ -8,15 +8,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.snackbar.Snackbar
 import com.matrix.githubbrowser.R
 import com.matrix.githubbrowser.data.models.ItemsEntity
 import com.matrix.githubbrowser.databinding.FragmentAddRepoBinding
 import com.matrix.githubbrowser.domain.utils.Status
 import com.matrix.githubbrowser.presentation.main.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay
-import java.lang.NullPointerException
 
 @AndroidEntryPoint
 class AddRepoFragment : Fragment(R.layout.fragment_add_repo) {
